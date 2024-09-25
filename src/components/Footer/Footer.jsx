eabc18388a31240ea8c8193407914618522cdb70
Filipa <filipamnmlopes@hotmail.com>
2024-09-25 13:36:42 +0100

    work on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <nav className="navbar">
+    <nav className="footer">
+      <img src="src/assets/logos/logo.png" alt="Icebreak Logo" width="200"/>
       <h5>Contact us</h5>
       <h5>Visit us</h5>
       <h5>Find us</h5>
