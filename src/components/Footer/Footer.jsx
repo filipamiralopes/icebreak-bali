f37c5c17f961052c81836dc0a407c3e54738fa3f
Filipa <filipamnmlopes@hotmail.com>
2024-09-26 20:28:44 +0100

    try to fix img import

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
+import logoBlack from "../../assets/logos/logo.png";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
   return (
     <nav className="footer">
-      <img src="src/assets/logos/logo.png" alt="Icebreak Logo" width="200"/>
+      <img src={logoBlack} alt="Icebreak Logo" width="200"/>
       <h5>Contact us</h5>
       <h5>Visit us</h5>
       <h5>Find us</h5>
