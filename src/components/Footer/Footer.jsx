5024ee321d56d55a6e870cfa583e03049331a597
Filipa <filipamnmlopes@hotmail.com>
2024-09-27 07:31:26 +0100

    work on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,52 @@
 import logoBlack from "../../assets/logos/logo.png";
+import contactIcon from "../../assets/icons/contact-icon.png";
+import locationIcon from "../../assets/icons/location-icon.png";
+import heartIcon from "../../assets/icons/heart-icon.png";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
   return (
     <nav className="footer">
-      <img src={logoBlack} alt="Icebreak Logo" width="200"/>
-      <h5>Contact us</h5>
-      <h5>Visit us</h5>
-      <h5>Find us</h5>
+      <img src={logoBlack} alt="Icebreak Logo" height="160px" />
+
+      <div className="info">
+        <div className="info-icon">
+          <h6>Contact us</h6>
+          <img
+            src={contactIcon}
+            alt="Contact icon"
+            height="50"
+          />
+        </div>
+        <a>+62 999 9999 999</a>
+      </div>
+
+      <div className="info">
+        <div className="info-icon">
+          <h6>Visit us</h6>
+          <img
+            src={locationIcon}
+            alt="Loction icon"
+            height="50"
+          />
+        </div>
+        <a href="https://maps.app.goo.gl/XN9GBsEXTbyePuGi7">
+          Main Road Bingin Uluatu Bali
+        </a>
+      </div>
+
+      <div className="info">
+      <div className="info-icon">
+          <h6>Follow us</h6>
+          <img
+            src={heartIcon}
+            alt="Heart icon"
+            height="50"
+          />
+        </div>
+        <a href="">@icebreakbali</a>
+      </div>
     </nav>
   );
 }
