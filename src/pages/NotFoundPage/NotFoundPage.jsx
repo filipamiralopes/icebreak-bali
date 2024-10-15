3972341400122e1af4974a052348fa8a25be5183
Filipa <filipamnmlopes@hotmail.com>
2024-10-15 12:11:53 +0100

    not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 import "./NotFoundPage.css";
-
 
 function NotFoundPage() {
   return (
-    <div>
+    <div className="not-found">
       <h1>Page Not Found</h1>
       <p>This page doesn't seem to exist</p>
+      <Link to="/">
+        <p>Back to homepage</p>
+      </Link>
     </div>
   );
 }
