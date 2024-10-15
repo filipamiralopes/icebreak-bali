d26dd1909b054245a62f9503b82d8df75d6babc7
Filipa <filipamnmlopes@hotmail.com>
2024-10-15 22:25:54 +0100

    switch footer to black

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,20 @@
 import logoBlack from "../../assets/logos/logo-black-long.png";
-import contactIcon from "../../assets/icons/contact-icon.png";
-import locationIcon from "../../assets/icons/location-icon.png";
-import heartIcon from "../../assets/icons/heart-icon.png";
+import logoGreen from "../../assets/logos/logo-green-long.png";
+import contactIcon from "../../assets/icons/contact-icon-rmb.png";
+import locationIcon from "../../assets/icons/location-icon-rmb.png";
+import heartIcon from "../../assets/icons/heart-icon-rmb.png";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
   return (
     <nav className="footer">
-      <img src={logoBlack} alt="Icebreak Logo" height="160px" />
+      <img src={logoGreen} alt="Icebreak Logo" height="100px" />
 
       <div className="info">
         <div className="info-icon">
           <h6>Contact us</h6>
-          <img
-            src={contactIcon}
-            alt="Contact icon"
-            height="50"
-          />
+          <img src={contactIcon} alt="Contact icon" height="50px" />
         </div>
         <a>+62 999 9999 999</a>
         <p>info@icebreak.com</p>
@@ -26,29 +23,33 @@ function Footer() {
       <div className="info">
         <div className="info-icon">
           <h6>Visit us</h6>
-          <img
-            src={locationIcon}
-            alt="Loction icon"
-            height="50"
-          />
+          <img src={locationIcon} alt="Loction icon" height="50px" />
         </div>
-        <a href="https://maps.app.goo.gl/XN9GBsEXTbyePuGi7" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://maps.app.goo.gl/XN9GBsEXTbyePuGi7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Main Road Bingin Uluatu Bali
         </a>
         <p>daily 7:30am - 7:30pm</p>
       </div>
 
       <div className="info">
-      <div className="info-icon">
+        <div className="info-icon">
           <h6>Follow us</h6>
-          <img
-            src={heartIcon}
-            alt="Heart icon"
-            height="50"
-          />
+          <img src={heartIcon} alt="Heart icon" height="50px" />
         </div>
-        <a href="https://www.instagram.com/icebreakbali/">@icebreakbali</a>
-        <a href="">facebook/iceabreak-bali.com</a>
+        <a
+          href="https://www.instagram.com/icebreakbali/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @icebreakbali
+        </a>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          facebook/iceabreak-bali.com
+        </a>
       </div>
     </nav>
   );
