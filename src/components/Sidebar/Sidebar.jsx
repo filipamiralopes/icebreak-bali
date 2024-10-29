f4fe7c3e133a20c75c212338838d62b14fe1a952
Filipa <filipamnmlopes@hotmail.com>
2024-10-29 15:29:46 +0800

    sidebar refactoring

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,32 @@
 import menuIcon from "../../assets/icons/menu-icon.png";
 import closeIcon from "../../assets/icons/close-icon.png";
+import arrowDownIcon from "../../assets/icons/arrow-down.png";
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isKnowMoreOpen, setIsKnowMoreOpen] = useState(false);
   const sidebarRef = useRef(null);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  const toggleKnowMore = () => {
+    setIsKnowMoreOpen(!isKnowMoreOpen);
+  };
+
   // Close sidebar when clicking outside of it
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (isOpen && sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-        setIsOpen(false);  // Close sidebar smoothly
+      if (
+        isOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
       }
     };
 
@@ -30,37 +40,54 @@ const Sidebar = () => {
     <>
       {!isOpen && (
         <div className="menu-icon" onClick={toggleSidebar}>
-          <img
-            src={menuIcon}
-            alt="Menu icon"
-            style={{ width: "50px", height: "auto" }}
-          />
+          <img src={menuIcon} alt="Menu icon" style={{ width: "50px", height: "auto" }} />
         </div>
       )}
 
       <div
         ref={sidebarRef}
-        className={`sidebar ${isOpen ? "open" : ""}`}  // Applies the "open" class for transition
+        className={`sidebar ${isOpen ? "open" : ""}`}
         onClick={(e) => e.stopPropagation()}
       >
         <div className="close-icon" onClick={toggleSidebar}>
-          <img
-            src={closeIcon}
-            alt="Close icon"
-            style={{ width: "70px", height: "auto" }}
-          />
+          <img src={closeIcon} alt="Close icon" style={{ width: "70px", height: "auto" }} />
         </div>
 
         <Link to="/" onClick={toggleSidebar}>
           <h4>Home</h4>
         </Link>
 
-        <Link to="/faq" onClick={toggleSidebar}>
-          <h4>FAQ</h4>
+        <Link to="/icebath" onClick={toggleSidebar}>
+          <h4>Ice Bath</h4>
         </Link>
 
-        <Link to="/about" onClick={toggleSidebar}>
-          <h4>About Us</h4>
+        <Link to="/redlight" onClick={toggleSidebar}>
+          <h4>Red Light Therapy</h4>
+        </Link>
+
+        <div className="know-more">
+          <h4 onClick={toggleKnowMore}>
+            Know More
+            <img
+              src={arrowDownIcon}
+              alt="Arrow icon"
+              className={`arrow-icon ${isKnowMoreOpen ? "open" : ""}`}
+            />
+          </h4>
+          {isKnowMoreOpen && (
+            <div className="submenu">
+              <Link to="/about" onClick={toggleSidebar}>
+                <h4>About Us</h4>
+              </Link>
+              <Link to="/faq" onClick={toggleSidebar}>
+                <h4>FAQ</h4>
+              </Link>
+            </div>
+          )}
+        </div>
+
+        <Link to="/contact" onClick={toggleSidebar}>
+          <h4>Contact Us</h4>
         </Link>
       </div>
     </>
